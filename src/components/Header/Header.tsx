a52153c521385a0f9ecf89af27d94a082393b2f2
PandaTrumpet <dsavontrumpet@gmail.com>
2025-03-06 22:14:47 +0100

    finish navigate

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import css from "./Header.module.css";
 import logo from "../../images/logo.png";
 import logo1440 from "../../images/logo_1440.png";
@@ -10,12 +10,17 @@ import { IoCloseOutline } from "react-icons/io5";
 import clsx from "clsx";
 const Header = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
+
   const handleToggle = () => {
     setIsMobile(!isMobile);
   };
+
   const [registered, setRegistered] = useState(true);
+  const buildLinkClass = ({ isActive }: { isActive: boolean }) => {
+    return clsx(css.link, isActive && css.active);
+  };
   return (
-    <div className={css.headerContainer}>
+    <header className={css.headerContainer}>
       <div className={css.headerMain}>
         <Link to="/">
           <div className={css.logoCont}>
@@ -28,6 +33,75 @@ const Header = () => {
             <p>E-Pharmacy</p>
           </div>
         </Link>
+        <nav>
+          <ul className={css.navMenu}>
+            <li className={css.homeLink}>
+              <NavLink
+                to="/"
+                // className={clsx(css.link, css.first)}
+                className={buildLinkClass}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={css.storeLink}>
+              <NavLink
+                to="/medicine-store"
+                // className={clsx(css.link, css.second)}
+                className={buildLinkClass}
+              >
+                Medicine store
+              </NavLink>
+            </li>
+            <li className={css.medicinelink}>
+              <NavLink to="/medicine" className={buildLinkClass}>
+                Medicine
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={css.desktopRegistered}>
+          {registered && (
+            <ul className={css.navRegisteredElementDesktop}>
+              <li>
+                <div className={css.marketIconCont}>
+                  <img
+                    src={market}
+                    alt="Shop icon"
+                    className={css.marketIcion}
+                  />
+
+                  <div className={css.quantityShop}>
+                    <p>0</p>
+                  </div>
+                </div>
+              </li>
+              <li className={css.i}>
+                <p>I</p>
+              </li>
+            </ul>
+          )}
+          <ul className={css.registerListDesktop}>
+            {registered ? (
+              <li>
+                <button className={css.logoutBtn}>Log out</button>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to="/register" className={css.registerBtn}>
+                    Register
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className={css.loginBtn}>
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
         {registered ? (
           <ul className={css.navRegisteredElement}>
             <li>
@@ -43,15 +117,8 @@ const Header = () => {
               <p>I</p>
             </li>
             <li>
-              <button
-                className={isMobile ? css.mobileClose : css.mobileMenuIcon}
-                onClick={handleToggle}
-              >
-                {isMobile ? (
-                  <IoCloseOutline size={24} />
-                ) : (
-                  <IoIosMenu size={24} />
-                )}
+              <button className={css.mobileMenuIcon} onClick={handleToggle}>
+                <IoIosMenu size={24} />
               </button>
             </li>
           </ul>
@@ -65,7 +132,7 @@ const Header = () => {
         )}
       </div>
       {isMobile && (
-        <div className={css.mobileMenu}>
+        <nav className={clsx(css.mobileMenu, isMobile && css.active)}>
           <div className={css.darkTileMobile}></div>
           <div className={css.mainTileMobile}>
             <button
@@ -75,29 +142,74 @@ const Header = () => {
               <IoCloseOutline size={24} />
             </button>
             <ul className={css.mobileMenuList}>
-              <li>
-                <Link to="/" className={clsx(css.link, css.first)}>
+              <li className={css.homeLink}>
+                <NavLink
+                  to="/"
+                  // className={clsx(css.link, css.first)}
+                  className={buildLinkClass}
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
-              <li>
-                <Link
+              <li className={css.storeLink}>
+                <NavLink
                   to="/medicine-store"
-                  className={clsx(css.link, css.second)}
+                  // className={clsx(css.link, css.second)}
+                  className={buildLinkClass}
                 >
                   Medicine store
-                </Link>
+                </NavLink>
               </li>
-              <li>
-                <Link to="/medicine" className={css.link}>
+              <li className={css.medicinelink}>
+                <NavLink to="/medicine" className={buildLinkClass}>
                   Medicine
-                </Link>
+                </NavLink>
               </li>
             </ul>
+            {registered ? (
+              <ul className={css.registerList}>
+                <li>
+                  <button className={css.logoutBtn}>Log out</button>
+                </li>{" "}
+              </ul>
+            ) : (
+              <ul className={css.registerList}>
+                <li>
+                  <Link to="/register" className={css.registerBtn}>
+                    Register
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className={css.loginBtn}>
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            )}
+            {/* <ul className={css.registerList}>
+              {registered ? (
+                <li>
+                  <button className={css.logoutBtn}>Log out</button>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link to="/register" className={css.registerBtn}>
+                      Register
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/login" className={css.loginBtn}>
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul> */}
           </div>
-        </div>
+        </nav>
       )}
-    </div>
+    </header>
   );
 };
 
