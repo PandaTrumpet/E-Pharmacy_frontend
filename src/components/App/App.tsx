109aac3f46686048d2f2b4d47712ef52e1411834
PandaTrumpet <dsavontrumpet@gmail.com>
2025-03-16 23:00:18 +0100

    add lazy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,67 +1,74 @@
+import { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import SharedLayout from "../SharedLayout/SharedLayout";
-import HomePage from "../../Pages/HomePage/HomePage";
-import MedicineStorePage from "../../Pages/MedicineStorePage/MedicineStorePage";
-import MedicinePage from "../../Pages/MedicinePage/MedicinePage";
-import ProductPage from "../../Pages/ProductPage/ProductPage";
-import LoginPage from "../../Pages/LoginPage/LoginPage";
-import RegisterPage from "../../Pages/RegisterPage/RegisterPage";
-import Description from "../Description/Description";
 import { Toaster } from "react-hot-toast";
-import ProductReviews from "../ProductReviews/ProductReviews";
-import LoginModal from "../LoginModal/LoginModal";
-import RegisterModal from "../RegisterModal/RegisterModal";
 import { useDispatch, useSelector } from "react-redux";
-import { selectModalType } from "../../redux/modal/selector";
-import SimpleModal from "../SimpleModal/SimpleModal";
-import CartPage from "../../Pages/CartPage/CartPage";
 import { AppDispatch } from "../../redux/store";
-import { useEffect } from "react";
 import { getOrders } from "../../redux/orders/operation";
 import { totalProductsCountSelector } from "../../redux/orders/selector";
 import { isLoggedSelector } from "../../redux/auth/selector";
+import { selectModalType } from "../../redux/modal/selector";
+import SimpleModal from "../SimpleModal/SimpleModal";
+import LoginModal from "../LoginModal/LoginModal";
+import RegisterModal from "../RegisterModal/RegisterModal";
 import PrivateRoute from "../PrivateRoute";
-import NotFoundPage from "../../Pages/NotFoundPage";
+import { lazy } from "react";
+const SharedLayout = lazy(() => import("../SharedLayout/SharedLayout"));
+const HomePage = lazy(() => import("../../Pages/HomePage/HomePage"));
+const MedicineStorePage = lazy(
+  () => import("../../Pages/MedicineStorePage/MedicineStorePage")
+);
+const MedicinePage = lazy(
+  () => import("../../Pages/MedicinePage/MedicinePage")
+);
+const ProductPage = lazy(() => import("../../Pages/ProductPage/ProductPage"));
+const CartPage = lazy(() => import("../../Pages/CartPage/CartPage"));
+const LoginPage = lazy(() => import("../../Pages/LoginPage/LoginPage"));
+const RegisterPage = lazy(
+  () => import("../../Pages/RegisterPage/RegisterPage")
+);
+const NotFoundPage = lazy(() => import("../../Pages/NotFoundPage"));
+const Description = lazy(() => import("../Description/Description"));
+const ProductReviews = lazy(() => import("../ProductReviews/ProductReviews"));
 
 const App = () => {
   const isLogged = useSelector(isLoggedSelector);
   const modalTypeSelect = useSelector(selectModalType);
   const totalProducts = useSelector(totalProductsCountSelector) || 0;
   const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
-    if (!isLogged) {
-      return;
-    } else {
+    if (isLogged) {
       const orderId = localStorage.getItem("orderId");
       if (orderId) {
         dispatch(getOrders());
       }
     }
-  }, [dispatch, totalProducts]);
+  }, [dispatch, totalProducts, isLogged]);
+
   return (
     <>
-      <Routes>
-        <Route element={<SharedLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/medicine-store" element={<MedicineStorePage />} />
-          <Route path="/medicine" element={<MedicinePage />} />
-          <Route path="/product/:productId" element={<ProductPage />}>
-            <Route path="description" element={<Description />} />
-            <Route path="reviews" element={<ProductReviews />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<SharedLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/medicine-store" element={<MedicineStorePage />} />
+            <Route path="/medicine" element={<MedicinePage />} />
+            <Route path="/product/:productId" element={<ProductPage />}>
+              <Route path="description" element={<Description />} />
+              <Route path="reviews" element={<ProductReviews />} />
+            </Route>
+            <Route
+              path="/cart"
+              element={
+                <PrivateRoute component={<CartPage />} redirectTo="/login" />
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
           </Route>
-
-          <Route
-            path="/cart"
-            element={
-              <PrivateRoute component={<CartPage />} redirectTo="/login" />
-            }
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Suspense>
 
       <Toaster position="top-center" toastOptions={{ duration: 1000 }} />
 
